48ee119f6d0292c1e9407611a05295823d857d22
Roko Svalina <roko.svalina@gmail.com>
2024-12-23 12:38:16 +0100

    Fixed illustration overlaping issue in map section, removed images from testimonials section ( still have to add two reviews ), the code for grid elements in hero section, removed scroll from when the mobile nav is open andadded scroll-padding on htm element also made every change responsive

--- script.js
+++ script.js
@@ -6,13 +6,49 @@ console.log(currentYear);
 yearEl.textContent = currentYear;
 
 // MOBILE NAVIGATION
-const btnNav = document.querySelector(".btn-mobile-nav");
+const btnNav = document.querySelector(".mobile-navigation-button");
+const menuBtn = document.querySelector(".menu-outline");
+const closeBtn = document.querySelector(".close-outline");
+const body = document.querySelector("body");
+const navLinks = document.querySelectorAll(".main-nav-link");
 const header = document.querySelector(".header-section");
 
 btnNav.addEventListener("click", function () {
   header.classList.toggle("nav-open");
 });
 
+// DISABLE SCROLL
+menuBtn.addEventListener("click", function () {
+  body.classList.add("scroll-disabled");
+  body.classList.remove("scroll-enable");
+});
+
+// OMOGUĆI SCROLL
+closeBtn.addEventListener("click", function () {
+  body.classList.remove("scroll-disabled");
+  body.classList.add("scroll-enable");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const targetId = link.getAttribute("href").substring(1);
+
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      // Pomicanje na odgovarajuću sekciju
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+
+      body.classList.remove("scroll-disabled");
+      body.classList.add("scroll-enable");
+    }
+  });
+});
 // // STICKY NAVIGATION
 const sectionHero = document.querySelector(".section-hero");
 const observerHero = new IntersectionObserver(
@@ -131,14 +167,23 @@ const markers = [
     popis radnih nedjelja možete vidjeti <a target="_blank" href="https://www.hocuknjigu.hr/radne-nedjelje-u-hocu-knjigu-knjizarama">ovdje</a>`,
   },
   {
-    locationName: "Supernova Garden Mall -Zagreb",
+    locationName: "Buzin Supernova -Zagreb",
     lat: 45.83615984477321,
     lng: 16.04624933932366,
-    address: "Ul. Rudolfa Kolaka 14, 10040, Zagreb",
+    address: " Av. Većeslava Holjevca 62, 10010, Buzin",
     workingHours: `<strong>Radno vrijeme:</strong><br>
     Ponedjeljak-nedjelja od 9:00 do 21:00<br>
     popis radnih nedjelja možete vidjeti <a target="_blank" href="https://www.hocuknjigu.hr/radne-nedjelje-u-hocu-knjigu-knjizarama">ovdje</a>`,
   },
+  // {
+  //   locationName: "Supernova Garden Mall -Zagreb",
+  //   lat: 45.83615984477321,
+  //   lng: 16.04624933932366,
+  //   address: "Ul. Rudolfa Kolaka 14, 10040, Zagreb",
+  //   workingHours: `<strong>Radno vrijeme:</strong><br>
+  //   Ponedjeljak-nedjelja od 9:00 do 21:00<br>
+  //   popis radnih nedjelja možete vidjeti <a target="_blank" href="https://www.hocuknjigu.hr/radne-nedjelje-u-hocu-knjigu-knjizarama">ovdje</a>`,
+  // },
   {
     locationName: "Hoću knjigu webshop -Zagreb",
     lat: 45.755487372288705,
@@ -146,7 +191,7 @@ const markers = [
     address: "Sveti Duh 6, Zagreb",
     workingHours: `<strong>Radno vrijeme:</strong><br>
     Ponedjeljak-petak od 7:00 do 15:00<br>
-    <a target="_blank" href="https://www.hocuknjigu.hr/">web-shop</a>`,
+    <a target="_blank" href="https://www.hocuknjigu.hr/proizvodi/knjige/publicistika/popularna-psihologija/energija-djetetove-proslosti">web-shop</a>`,
   },
   // VARAZDIN
   {
@@ -160,15 +205,15 @@ const markers = [
 Nedjelja - ne radi`,
   },
   // KOPRIVNICA
-  {
-    locationName: "Supernova Koprivnica",
-    lat: 46.14745270083373,
-    lng: 16.834661721278163,
-    address: "Gospodarska ul. 1, 48000, Koprivnica",
-    workingHours: `<strong>Radno vrijeme:</strong><br>
-    Ponedjeljak-nedjelja od 9:00 do 21:00<br>
-    popis radnih nedjelja možete vidjeti <a target="_blank" href="https://www.hocuknjigu.hr/radne-nedjelje-u-hocu-knjigu-knjizarama">ovdje</a>`,
-  },
+  // {
+  //   locationName: "Supernova Koprivnica",
+  //   lat: 46.14745270083373,
+  //   lng: 16.834661721278163,
+  //   address: "Gospodarska ul. 1, 48000, Koprivnica",
+  //   workingHours: `<strong>Radno vrijeme:</strong><br>
+  //   Ponedjeljak-nedjelja od 9:00 do 21:00<br>
+  //   popis radnih nedjelja možete vidjeti <a target="_blank" href="https://www.hocuknjigu.hr/radne-nedjelje-u-hocu-knjigu-knjizarama">ovdje</a>`,
+  // },
   // PULA
   {
     locationName: "Max City centar -Pula",
@@ -248,7 +293,7 @@ async function initMap() {
   infoWindow = new google.maps.InfoWindow();
 
   for (let i = 0; i < markers.length; i++) {
-    const pinSvgString = `<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="32.2" height="42.5" style="overflow:visible;enable-background:new 0 0 32.2 42.5"><path d="m.2 2.6 29-2.6s2.2-.2 2.8 2.7l.1 35.4s-.1 2.1-2.3 2.8c0 0-.4.8-1 1.1 0 0-.1.4-2.3.4H0L.2 2.6z" style="fill:#5BBE83"/><path d="M16.9 18.1c.1.1.4.2.4.5.1.4 0 .8-.4 1l-1.4.6H15c-.1-.1-.4-.2-.4-.4v-.6c.1-.1.2-.4.4-.4l1.4-.6c.2-.2.4-.2.5-.1zM29.3 0 .3 2.6v17.3c.5-.4 1.2-.4 1.7 0V3.7h25.6c1.1 0 1.9 1 1.9 2v33.6c0 1.1-.8 2-1.9 2h-1.7l-.1.2c-.1.2-.2.5-.5.7h2.3c1 0 1.9-.5 2.4-1.4a3.3 3.3 0 0 0 2.3-3V3.1C32 1.4 30.8 0 29.3 0zm1.8 38c0 .7-.4 1.3-.8 1.7V5.8a3 3 0 0 0-2.9-3h-16l17.9-1.6c1.1 0 1.9 1 1.9 2L31.1 38zM5 20.6c.5 0 .8.4.8 1v10.1c0 .5-.4 1-.8 1-.5 0-.8-.4-.8-1v-3.5H2v3.5c0 .5-.4 1-.8 1-.5 0-.8-.4-.8-1V21.6c0-.5.4-1 .8-1 .5 0 .8.4.8 1v4.8h2.2v-4.8c0-.6.4-1 .8-1zm7.2 8.9v.4l-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6-1.1.1-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7V23.7l.2-1.4c.1-.4.4-.6.6-.8.5-.5 1.1-.7 1.8-.7l1 .1c.4.1.6.4.8.6.2.2.5.6.6 1l.2 1.4v5.6zm-1.9 1.1.1-.2.1-.4v-6.3l-.1-.6-.2-.4-.4-.1H9.2l-.4.1-.1.2-.1.7v6.3l.1.4.1.2.2.1.5.1.5-.1h.3zm8.3-1.2v.4l-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6c-.4.2-.8.2-1.2.2l-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7v-6.8l.2-1.4c.1-.4.4-.6.6-.8.5-.5 1.1-.7 1.8-.7l1 .1c.4.1.6.4.8.6.2.2.5.6.6 1l.2 1.4-.2.6c-.4.4-.8.4-1.2 0-.2-.1-.2-.4-.2-.6l-.1-.6-.2-.4-.4-.1H15.6l-.4.1-.2.4-.1.7v6.3l.1.4.1.2.2.1.5.1.5-.1.2-.1.2-.5v-.6l.1-.4.2-.4.5-.1.5.1c.4 0 .6.3.6.5zm6.5.5-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6c-.4.2-.8.2-1.2.2l-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7V21.7c0-.5.4-1 .8-1 .5 0 .8.4.8 1V30.1l.1.4.1.2.2.1c.2.2.4.2.5.2l.5-.1.2-.1.2-.5v-8.5c0-.5.4-1 .8-1 .5 0 .8.4.8 1v7.9c.3-.2.3-.1.3.2zm-20.5 12c0 .1-.1.4-.4.4H4c-.2 0-.5-.1-.6-.4l-1.6-3.2-.5.8v2.2c0 .4-.2.7-.6.7a1 1 0 0 1-.5-.2l-.2-.6v-7.4c0-.4.2-.7.6-.7l.5.2.2.5v2.6l1.8-3 .4-.4.5.1c.1.1.2.2.2.5l-.1.5-1.6 2.8 1.9 4.1c.2.1.2.4.2.5zm3.9-8.4c.4 0 .6.4.6.7v7.6h-.8.1l-.2-.2-2-4.1v4.2c0 .4-.2.7-.6.7-.4 0-.6-.4-.6-.7v-7.6h.9l1.8 4.4v-4.2c.2-.5.5-.7.8-.8zm5.2 6.8v.5l-.1.5-.4.5-.5.5-1 .2c-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5-.2-.1-.4-.4-.4-.6l-.1-.6v-.7l.1-.4.2-.2.4-.1.4.1.1.2v.7l.1.2.1.2.1.1.4.1.4-.1.1-.1.1-.2v-6.5c0-.4.2-.7.6-.7l.5.2.2.5v5.9l.2.5zm1.4-6.8c.1 0 .4 0 .5.2l.2.5v7.6c0 .1 0 .4-.2.5-.1.1-.2.2-.5.2-.4 0-.6-.4-.6-.7v-7.6c0-.4.3-.7.6-.7zm5.3 6.8v.5l-.1.5-.4.5-.5.5-1 .2c-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5l-.4-.5-.1-.5v-5c0-.4 0-.7.2-1.1.1-.2.2-.5.5-.6.4-.4.8-.6 1.3-.5.5 0 1 .1 1.3.5l.5.6.2 1.1c0 .1 0 .4-.2.5-.2.2-.6.2-1 0a1 1 0 0 1-.2-.5v-.5l.1-.3-.2-.1h-.4l-.2.1-.2.2-.1.5v4.9l.1.2.1.1.4.1.4-.1.1-.1.1-.2.1-.2V39h-.5c-.4 0-.6-.4-.6-.7 0-.4.2-.7.6-.7h1.8V40c-.2.1-.2.2-.2.3zm4.6.1v.5l-.1.5-.4.5c-.4.5-1 .6-1.4.6-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5l-.4-.5-.1-.5v-6.6c0-.4.2-.7.6-.7l.5.2.2.5v6.5l.1.2.1.1.4.1.4-.1.1-.1.2-.4v-6.4c0-.4.2-.7.6-.7.4 0 .6.4.6.7V40c.2.2.1.2.1.4z" style="fill:#020202"/></svg>`;
+    const pinSvgString = `<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="32.2" height="42.5" style="overflow:visible;enable-background:new 0 0 32.2 42.5"><path d="m.2 2.6 29-2.6s2.2-.2 2.8 2.7l.1 35.4s-.1 2.1-2.3 2.8c0 0-.4.8-1 1.1 0 0-.1.4-2.3.4H.6a.7.7 0 0 1-.7-.7L.2 2.6z" style="fill:#7ccb9c"/><path d="M16.9 18.1c.1.1.4.2.4.5.1.4 0 .8-.4 1l-1.4.6H15c-.1-.1-.4-.2-.4-.4v-.6c.1-.1.2-.4.4-.4l1.4-.6c.2-.2.4-.2.5-.1zM29.3 0 .3 2.6v17.3c.5-.4 1.2-.4 1.7 0V3.7h25.6c1.1 0 1.9 1 1.9 2v33.6c0 1.1-.8 2-1.9 2h-1.7l-.1.2c-.1.2-.2.5-.5.7h2.3c1 0 1.9-.5 2.4-1.4a3.3 3.3 0 0 0 2.3-3V3.1C32 1.4 30.8 0 29.3 0zm1.8 38c0 .7-.4 1.3-.8 1.7V5.8a3 3 0 0 0-2.9-3h-16l17.9-1.6c1.1 0 1.9 1 1.9 2L31.1 38zM5 20.6c.5 0 .8.4.8 1v10.1c0 .5-.4 1-.8 1-.5 0-.8-.4-.8-1v-3.5H2v3.5c0 .5-.4 1-.8 1-.5 0-.8-.4-.8-1V21.6c0-.5.4-1 .8-1 .5 0 .8.4.8 1v4.8h2.2v-4.8c0-.6.4-1 .8-1zm7.2 8.9v.4l-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6-1.1.1-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7V23.7l.2-1.4c.1-.4.4-.6.6-.8.5-.5 1.1-.7 1.8-.7l1 .1c.4.1.6.4.8.6.2.2.5.6.6 1l.2 1.4v5.6zm-1.9 1.1.1-.2.1-.4v-6.3l-.1-.6-.2-.4-.4-.1H9.2l-.4.1-.1.2-.1.7v6.3l.1.4.1.2.2.1.5.1.5-.1h.3zm8.3-1.2v.4l-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6c-.4.2-.8.2-1.2.2l-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7v-6.8l.2-1.4c.1-.4.4-.6.6-.8.5-.5 1.1-.7 1.8-.7l1 .1c.4.1.6.4.8.6.2.2.5.6.6 1l.2 1.4-.2.6c-.4.4-.8.4-1.2 0-.2-.1-.2-.4-.2-.6l-.1-.6-.2-.4-.4-.1H15.6l-.4.1-.2.4-.1.7v6.3l.1.4.1.2.2.1.5.1.5-.1.2-.1.2-.5v-.6l.1-.4.2-.4.5-.1.5.1c.4 0 .6.3.6.5zm6.5.5-.1.6-.2.7c-.1.2-.2.5-.5.7l-.7.6c-.4.2-.8.2-1.2.2l-1.2-.2a3.7 3.7 0 0 1-1.2-1.3l-.2-.7V21.7c0-.5.4-1 .8-1 .5 0 .8.4.8 1V30.1l.1.4.1.2.2.1c.2.2.4.2.5.2l.5-.1.2-.1.2-.5v-8.5c0-.5.4-1 .8-1 .5 0 .8.4.8 1v7.9c.3-.2.3-.1.3.2zm-20.5 12c0 .1-.1.4-.4.4H4c-.2 0-.5-.1-.6-.4l-1.6-3.2-.5.8v2.2c0 .4-.2.7-.6.7a1 1 0 0 1-.5-.2l-.2-.6v-7.4c0-.4.2-.7.6-.7l.5.2.2.5v2.6l1.8-3 .4-.4.5.1c.1.1.2.2.2.5l-.1.5-1.6 2.8 1.9 4.1c.2.1.2.4.2.5zm3.9-8.4c.4 0 .6.4.6.7v7.6h-.8.1l-.2-.2-2-4.1v4.2c0 .4-.2.7-.6.7-.4 0-.6-.4-.6-.7v-7.6h.9l1.8 4.4v-4.2c.2-.5.5-.7.8-.8zm5.2 6.8v.5l-.1.5-.4.5-.5.5-1 .2c-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5-.2-.1-.4-.4-.4-.6l-.1-.6v-.7l.1-.4.2-.2.4-.1.4.1.1.2v.7l.1.2.1.2.1.1.4.1.4-.1.1-.1.1-.2v-6.5c0-.4.2-.7.6-.7l.5.2.2.5v5.9l.2.5zm1.4-6.8c.1 0 .4 0 .5.2l.2.5v7.6c0 .1 0 .4-.2.5-.1.1-.2.2-.5.2-.4 0-.6-.4-.6-.7v-7.6c0-.4.3-.7.6-.7zm5.3 6.8v.5l-.1.5-.4.5-.5.5-1 .2c-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5l-.4-.5-.1-.5v-5c0-.4 0-.7.2-1.1.1-.2.2-.5.5-.6.4-.4.8-.6 1.3-.5.5 0 1 .1 1.3.5l.5.6.2 1.1c0 .1 0 .4-.2.5-.2.2-.6.2-1 0a1 1 0 0 1-.2-.5v-.5l.1-.3-.2-.1h-.4l-.2.1-.2.2-.1.5v4.9l.1.2.1.1.4.1.4-.1.1-.1.1-.2.1-.2V39h-.5c-.4 0-.6-.4-.6-.7 0-.4.2-.7.6-.7h1.8V40c-.2.1-.2.2-.2.3zm4.6.1v.5l-.1.5-.4.5c-.4.5-1 .6-1.4.6-.4 0-.6 0-1-.2-.2-.1-.4-.2-.5-.5l-.4-.5-.1-.5v-6.6c0-.4.2-.7.6-.7l.5.2.2.5v6.5l.1.2.1.1.4.1.4-.1.1-.1.2-.4v-6.4c0-.4.2-.7.6-.7.4 0 .6.4.6.7V40c.2.2.1.2.1.4z" style="fill:#020202"/></svg>`;
     const pinSvg = parser.parseFromString(
       pinSvgString,
       "image/svg+xml"
